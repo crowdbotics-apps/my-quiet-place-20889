6cb275aed7b3aa635b7c029b09413b95ffa32e5c
Crowdbotics <team@crowdbotics.com>
2020-10-02 17:18:11 +0000

    Update models

--- backend/my_quiet_place_20889/wsgi.py
+++ backend/my_quiet_place_20889/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_quiet_place_20889.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_quiet_place_20889.settings")
 
 application = get_wsgi_application()
